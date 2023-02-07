1926198998f59033471c5b2c1322989b299d33ee
jpmcga <jpaulmcgann@gmail.com>
2023-02-07 10:06:53 -0500

    Fix dependencies for ArchR.

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,53 @@
 FROM 812206152185.dkr.ecr.us-west-2.amazonaws.com/latch-base:dd8f-main
 
-
 # Install R
 RUN apt-get update -y && \
     apt-get install -y software-properties-common && \
     add-apt-repository "deb http://cloud.r-project.org/bin/linux/debian buster-cran40/" && \
-    apt-get install -y r-base r-base-dev libxml2-dev libcurl4-openssl-dev libssl-dev wget
+    apt-get install -y \
+        r-base \
+        r-base-dev \
+        apt-transport-https \
+        build-essential \
+        curl \
+        gfortran \
+        git \
+        libatlas-base-dev \
+        libbz2-dev \        
+        libcurl4-openssl-dev \
+        libfontconfig1-dev \
+        libfreetype6-dev \
+        libgit2-dev \
+        libgsl-dev \
+        libicu-dev \
+        liblzma-dev \
+        libpango-1.0-0 \
+        libpangocairo-1.0-0 \
+        libpcre3-dev \
+        libssl-dev \
+        libtcl8.6 \
+        libtiff5 \
+        libtk8.6 \
+        libxml2-dev \
+        libxt-dev \
+        libx11-dev \
+        locales \
+        make \
+        pandoc \
+        tzdata \
+        zlib1g-dev        
+
+# Have to install devtools like this; see https://stackoverflow.com/questions/20923209, also cairo
+RUN apt-get install -y r-cran-devtools libcairo2-dev
 
 # Install packages
-RUN R -e "install.packages('devtools')"
-RUN R -e "install.packages('BiocManager')"
+RUN R -e "install.packages('Cairo', dependencies = TRUE)"
+RUN R -e "install.packages('BiocManager', dependencies = TRUE)"
 RUN R -e "devtools::install_github('GreenleafLab/ArchR', ref='master', repos = BiocManager::repositories())"
-RUN R -e "library(ArchR); ArchR::installExtraPackages()"
+RUN R -e "library('ArchR'); ArchR::installExtraPackages()"
 
-RUN R -e "BiocManager::install("BSgenome.Mmusculus.UCSC.mm10")"
-RUN R -e "BiocManager::install("BSgenome.Hsapiens.UCSC.hg38")"
-
+RUN R -e "BiocManager::install('BSgenome.Mmusculus.UCSC.mm10')"
+RUN R -e "BiocManager::install('BSgenome.Hsapiens.UCSC.hg38')"
 
 # STOP HERE:
 # The following lines are needed to ensure your build environement works
@@ -26,9 +58,3 @@ ARG tag
 ENV FLYTE_INTERNAL_IMAGE $tag
 WORKDIR /root
 
-# RUN apt install -y dirmngr apt-transport-https ca-certificates software-properties-common gnupg2
-# RUN apt-key adv --keyserver keyserver.ubuntu.com --recv-key '95C0FAF38DB3CCAD0C080A7BDC78B2DDEABC47B7'
-# RUN add-apt-repository 'deb https://cloud.r-project.org/bin/linux/debian buster-cran40/'
-# RUN apt update
-# RUN apt install -y r-base build-essential
-# RUN apt-get install libcurl4-openssl-dev
--- wf/__init__.py
+++ wf/__init__.py
@@ -1,5 +1,6 @@
-"""
-"""
+''' Short workflow for converting CellRanger output (fragments.tss.gz) into ArchR
+objects for downstream analysis.
+'''
 import subprocess
 from enum import Enum
 
@@ -12,24 +13,25 @@ from latch.types import (
     LatchParameter,
 )
 
+
 class Genome(Enum):
-    mm10 = "mm10"
-    hg38 = "hg38"
+    mm10 = 'mm10'
+    hg38 = 'hg38'
 
 @medium_task
 def archr_task(
     fragments_file: LatchFile,
     run_id: str,
-    genome: str,
+    genome: Genome,
     threads: int = 24,
     tile_size: int = 5000,
     min_TSS: float = 2,
     min_frags: int = 0
-) -> (LatchFile, LatchDir):
+) -> LatchDir:
     
     _archr_cmd = [
-        "Rscript",
-        "test.R",
+        'Rscript',
+        'test.R',
         f'{fragments_file.local_path}',
         f'{run_id}',
         f'{genome}',
@@ -41,63 +43,68 @@ def archr_task(
     
     subprocess.run(_archr_cmd)
 
-    return (
-        LatchFile(
-            f'/root/{run_id}.arrow',
-            f'latch:///archr_outs/{run_id}/{run_id}.arrow'
-        ),
-        LatchDir(
-            f'/root/ArchRProject',
-            f'latch:///archr_outs/{run_id}/ArchRProject'
-        )
+    subprocess.run(['mkdir', f'{run_id}_archr_outs'])
+    _mv_cmd = [
+        'mv',
+        f'/root/{run_id}.arrow',
+        f'/root/{run_id}_ArchRProject',
+        f'{run_id}_archr_outs'
+    ]
+
+    subprocess.run(_mv_cmd)
+
+    return LatchDir(
+        f'/root/{run_id}_archr_outs',
+        f'latch:///archr_outs/{run_id}_archr_outs'
     )
 
+
 metadata = LatchMetadata(
-    display_name="archr",
+    display_name='archr',
     author=LatchAuthor(
-        name="James McGann",
-        email="jpaulmcgann@gmail.com",
-        github="github.com/jpmcga",
+        name='James McGann',
+        email='jpaulmcgann@gmail.com',
+        github='github.com/jpmcga',
     ),
-    repository="https://github.com/jpmcga/archr_latch/",
-    license="MIT",
+    repository='https://github.com/jpmcga/archr_latch/',
+    license='MIT',
     parameters={
-        "fragments_file": LatchParameter(
-            display_name="Fragments file",
-            description="fragments.tsv.gz from CellRanger outs folder",
+        'fragments_file': LatchParameter(
+            display_name='Fragments file',
+            description='fragments.tsv.gz from CellRanger outs folder',
             batch_table_column=True, 
         ),
-        "run_id": LatchParameter(
-            display_name="run id",
-            description="run id with optional suffix, default to Dxxxxx_NGxxxxx",
+        'run_id': LatchParameter(
+            display_name='run id',
+            description='run id with optional prefix, default to Dxxxxx_NGxxxxx',
             batch_table_column=True,
-            placeholder="Dxxxxx_NGxxxxx"
+            placeholder='Dxxxxx_NGxxxxx'
         ),
-        "genome": LatchParameter(
-            display_name="genome",
-            description="select reference genome",
+        'genome': LatchParameter(
+            display_name='genome',
+            description='select reference genome',
             batch_table_column=True,
         ),
-        "threads": LatchParameter( # Might want to set a rule here
-            display_name="threads",
-            description="number of threads to be used; max 24",
-            batch_table_column=True,
-            hidden=True
-        ),
-        "tile_size": LatchParameter(
-            display_name="tile size",
+        'threads': LatchParameter( # Might want to set a rule here
+            display_name='threads',
+            description='number of threads to be used; max 24',
             batch_table_column=True,
             hidden=True
         ),
-        "min_TSS": LatchParameter(
-            display_name="minimum TSS",
-            description="minumum TSS for filtering",
+        'tile_size': LatchParameter(
+            display_name='tile size',
             batch_table_column=True,
             hidden=True
         ),
-        "min_frags": LatchParameter(
-            display_name="minumum fragments",
-            description="minumum TSS for filtering",
+        'min_TSS': LatchParameter(
+            display_name='minimum TSS',
+            description='minumum TSS for filtering',
+            batch_table_column=True,
+            hidden=True
+        ),
+        'min_frags': LatchParameter(
+            display_name='minumum fragments',
+            description='minumum TSS for filtering',
             batch_table_column=True,
             hidden=True
         ),        
@@ -115,14 +122,15 @@ def archr_workflow(
     tile_size: int,
     min_TSS: float,
     min_frags: int,
-) -> (LatchFile, LatchDir):
-    """Pipeline for converting fragment.tsv.gz files from 10x cellranger to 
+) -> LatchDir:
+    '''
+    Pipeline for converting fragment.tsv.gz files from 10x cellranger to 
     ArchR .arrow files and ArchRProject folders.
 
-    For data from DBiT-seq for spatially-resolved epigenomics:
+    For data from DBiT-seq for spatially-resolved epigenomics.
 
     - See Deng, Y. et al 2022.
-    """
+    '''
 
     return archr_task(
         fragments_file=fragments_file,
@@ -133,8 +141,3 @@ def archr_workflow(
         min_TSS=min_TSS,
         min_frags=min_frags
     )
-
-
-
-
-
