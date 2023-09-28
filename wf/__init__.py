df34f059b6b17b98de9e0bb9782d31433fb60949
jpmcga <jpaulmcgann@gmail.com>
2023-09-28 16:27:32 -0400

    fix: Latch about

--- wf/__init__.py
+++ wf/__init__.py
@@ -208,13 +208,13 @@ def archrproject_workflow(
     run_table_id: str="761",
     project_table_id: str="779"
 ) -> LatchDir:
+    '''Workflow for converting fragment.tsv.gz files to ArchRProjects.
 
-  '''Workflow for converting fragment.tsv.gz files to ArchRProjects.
     # create ArchRProject
-    <br>
-    <br>
+    
     **create ArchRProject** is a [latch.bio](https://latch.bio/) workflow for generating R objects and data for downstream analysis of epigenomic [DBiT-seq](https://www.nature.com/articles/s41586-022-05094-1) experiments.  Provided fragments from a single-cell ATAC-seq preprocessing and alignment workflow and spatial information, **create ArchRProjectr** performs the heavy computational steps of ArchR and Seurat and returns files that can be easily input into custom scripts for more neuanced analysis without the need to perform intensive computation.
     The workflow utilizes [ArchR](https://www.archrproject.com/articles/Articles/tutorial.html) to perform epigenomic single-cell analysis and [Seurat](https://satijalab.org/seurat/) to spatially align the data.  The workflow can take data from either a single tissue-sample analyzed via DBiT-seq or multiple tissue-samples; in ATX parlance, tissue-samples analyzed via DBIT-seq are termed 'Runs'.  All Runs input to **create ArchRProject** are merged into a single ArchRProject for analysis.  
+    
     ## Inputs
     All input files for **create ArchRProject** must be on the latch.bio [file system](https://wiki.latch.bio/wiki/data/overview).  Each run in the workflow takes the following parameters,
     * [fragments.tsv.gz file](https://support.10xgenomics.com/single-cell-atac/software/pipelines/latest/output/fragments): A BED-like, tab-delimited file in which each row contains an ATAC-seq fragment
@@ -230,6 +230,7 @@ def archrproject_workflow(
     * LSI varFeatures: An integer used as input to the `varFeatures` parameter of the `addIterativeLSI` function in [ArchR](https://www.archrproject.com/reference/addIterativeLSI.html);
     * clustering resolution: A decimal value used as input to the `resolution` parameter of the `addClusters` function in [ArchR](https://www.archrproject.com/reference/addClusters.html).
     > The Project also takes a series of single-value parameters that can be found under the 'Hidden Parameters' dropdown; these parameters are less commonly varied inputs to ArchR functions.
+    
     ## Running the workflow (_in progress_)
     The **create ArchRProject** workflow can be found in the [Workflows](https://wiki.latch.bio/workflows/overview) module in your latch.bio workspace. For access to an ATX-collaborator workspace, please contact your AtlasXomics Support Scientist or email support@atlasxomics.com.  See [here](https://wiki.latch.bio/workflows/overview) for general instructions for running workflows in latch.bio.
     1. Navigate to the **optimize archr** workflow in the Workflows module in your latch.bio workspace.  Ensure you are on the 'Parameters' tab of the workflow.
@@ -239,6 +240,7 @@ def archrproject_workflow(
     5. Click the 'Launch Workflow' button on the bottom-right of the parameters page.  This will automatically navigate you to the Executions tab of the workflow.
     6. From the Executions tab, you can view the status of the launched workflow.  Once the workflow has completed running, the status will change to 'Succeeded'; if the workflow has the status 'Failed', please contact an AtlasXomics Support Scientist.  You can click on the workflow execution to view a more granular workflow status and see output logs.
     7. Workflow outputs are loaded into the latch.bio [Data module](https://wiki.latch.bio/wiki/data/overview) in the `ArchRProjects` directory.
+
     ## Outputs (_in progress_)
     Outputs from **create ArchRProject** are loaded into latch.bio [Data module](https://wiki.latch.bio/wiki/data/overview) in the `ArchRProjects` directory.
     * ArchRProject/
@@ -272,8 +274,9 @@ def archrproject_workflow(
     
     ## Support
     Questions? Comments?  Contact support@atlasxomics.com or post in AtlasXomics [Discord](https://discord.com/channels/1004748539827597413/1005222888384770108).
+    
     '''
-  archr_project = archr_task(
+    archr_project = archr_task(
         runs=runs,
         project_name=project_name,
         genome=genome,
@@ -286,14 +289,15 @@ def archrproject_workflow(
         clustering_resolution=clustering_resolution,
         umap_mindist=umap_mindist
     )
-  upload_to_registry(
+   
+    upload_to_registry(
         runs=runs,
         archr_project=archr_project,
         run_table_id=run_table_id,
         project_table_id=project_table_id
     )
-  
-  return archr_project
+   
+    return archr_project
 
 LaunchPlan(
     archrproject_workflow,
