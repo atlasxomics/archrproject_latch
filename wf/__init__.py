5f458d618ee8ea961a37c8154201063d2101964f
jpmcga <jpaulmcgann@gmail.com>
2023-11-07 14:35:33 -0500

    chore: quick style update

--- wf/__init__.py
+++ wf/__init__.py
@@ -21,9 +21,11 @@ from latch.types import (
 
 from wf.upload_to_registry import upload_to_registry, Run
 
+
 class Genome(Enum):
     mm10 = 'mm10'
     hg38 = 'hg38'
+
 
 @custom_task(cpu=62, memory=384, storage_gib=500)
 def archr_task(
@@ -39,7 +41,7 @@ def archr_task(
     clustering_resolution: float,
     umap_mindist: float
 ) -> LatchDir:
-    
+
     _archr_cmd = [
         'Rscript',
         '/root/wf/archr_objs.R',
@@ -57,15 +59,15 @@ def archr_task(
 
     runs = [
         (
-        f'{run.run_id},'
-        f'{run.fragments_file.local_path},'
-        f'{run.condition},'
-        f'{run.positions_file.local_path},'
-        f'{run.spatial_dir.local_path},'
+            f'{run.run_id},'
+            f'{run.fragments_file.local_path},'
+            f'{run.condition},'
+            f'{run.positions_file.local_path},'
+            f'{run.spatial_dir.local_path},'
         )
-    for run in runs
+        for run in runs
     ]
-    
+
     _archr_cmd.extend(runs)
     subprocess.run(_archr_cmd)
 
@@ -95,6 +97,7 @@ def archr_task(
         f'latch:///ArchRProjects/{out_dir}'
     )
 
+
 metadata = LatchMetadata(
     display_name='create ArchRProject',
     author=LatchAuthor(
@@ -112,9 +115,9 @@ metadata = LatchMetadata(
                          tissue position file for filtering on/off tissue, \
                          spatial folder for SpatialDimPlot.',
             batch_table_column=True,
-            samplesheet=True 
+            samplesheet=True
         ),
-        'project_name' : LatchParameter(
+        'project_name': LatchParameter(
             display_name='project name',
             description='Name of output directory in archr_outs/',
             batch_table_column=True,
@@ -132,7 +135,7 @@ metadata = LatchMetadata(
             batch_table_column=True,
         ),
         'tile_size': LatchParameter(
-            display_name='tile size', 
+            display_name='tile size',
             description='The size of the tiles used for binning counts in the \
                         TileMatrix.',
             batch_table_column=True,
@@ -157,56 +160,60 @@ metadata = LatchMetadata(
             description='iterations parameter from addIterativeLSI function.',
             batch_table_column=True,
             hidden=True
-        ),                
+        ),
         'lsi_resolution': LatchParameter(
             display_name='LSI resolution',
             description='resolution parameter from \
                         addIterativeLSI/clusterParams function.',
             batch_table_column=True
-        ),                
+        ),
         'lsi_varfeatures': LatchParameter(
             display_name='LSI varFeatures',
             description='varFeatures parameter from addIterativeLSI function.',
             batch_table_column=True
-        ),              
+        ),
         'clustering_resolution': LatchParameter(
             display_name='clustering resolution',
             description='resolution parameter from addClusters function.',
             batch_table_column=True
-        ),              
+        ),
         'umap_mindist': LatchParameter(
             display_name='UMAP minimum distance',
             description='minDist parameter from addUMAP function.',
             batch_table_column=True,
             hidden=True
-        ),         
+        ),
         'run_table_id': LatchParameter(
             display_name='Registry Table ID',
-            description='The runs will be updated in Registry with its corresponding condition, spatial directory, condition, and location of the output archR project.'
+            description='The runs will be updated in Registry with its \
+                        corresponding condition, spatial directory, \
+                        condition, and location of the output archR project.'
         ),
         'project_table_id': LatchParameter(
             display_name='The ID of the SOWs Registry table',
-            description='The ArchR project will be inserted into the SOW table for the corresponding runs.'
-        )   
+            description='The ArchR project will be inserted into the SOW \
+                        table for the corresponding runs.'
+        )
     },
     tags=[],
 )
+
 
 @workflow(metadata)
 def archrproject_workflow(
     runs: List[Run],
     genome: Genome,
     project_name: str,
-    tile_size: int=5000,
-    min_TSS: float=2.0,
-    min_frags: int=0,
-    lsi_iterations: int=2,
-    lsi_resolution: float=0.5,
-    lsi_varfeatures: int=25000,
-    clustering_resolution: float=1.0,
-    umap_mindist: float=0.0,
-    run_table_id: str="761",
-    project_table_id: str="779"
+    tile_size: int = 5000,
+    min_TSS: float = 2.0,
+    min_frags: int = 0,
+    lsi_iterations: int = 2,
+    lsi_resolution: float = 0.5,
+    lsi_varfeatures: int = 25000,
+    clustering_resolution: float = 1.0,
+    umap_mindist: float = 0.0,
+    run_table_id: str = "761",
+    project_table_id: str = "779"
 ) -> LatchDir:
     '''Workflow for converting fragment.tsv.gz files to ArchRProjects.
 
@@ -271,10 +278,10 @@ def archrproject_workflow(
     ## Next Steps
     Analysis can be performed locally or in a latch.bio [Pod](https://wiki.latch.bio/wiki/pods/overview).  For access to ATX-specific Pods, please contact your AtlasXomics Support Scientist.  
     Output from **create ArchRProject** can be provided to the **atlasShiny** workflow to create input for a DBiT-seq-specific R Shiny App.  For access to this workflow and app, please contact your AtlasXomics Support Scientist.
-    
+
     ## Support
     Questions? Comments?  Contact support@atlasxomics.com or post in AtlasXomics [Discord](https://discord.com/channels/1004748539827597413/1005222888384770108).
-    
+
     '''
     archr_project = archr_task(
         runs=runs,
@@ -289,32 +296,39 @@ def archrproject_workflow(
         clustering_resolution=clustering_resolution,
         umap_mindist=umap_mindist
     )
-   
+
     upload_to_registry(
         runs=runs,
         archr_project=archr_project,
         run_table_id=run_table_id,
         project_table_id=project_table_id
     )
-   
+
     return archr_project
+
 
 LaunchPlan(
     archrproject_workflow,
     'defaults',
     {
-    'runs' : [
-        Run(
-            'default',
-            LatchFile('latch:///atac_outs/demo/outs/demo_fragments.tsv.gz'),
-            'demo',
-            LatchDir('latch:///spatials/demo/spatial'),
-            LatchFile('latch:///spatials/demo/spatial/tissue_positions_list.csv'),
+        'runs': [
+            Run(
+                'default',
+                LatchFile(
+                    'latch:///atac_outs/demo/outs/demo_fragments.tsv.gz'
+                ),
+                'demo',
+                LatchDir(
+                    'latch:///spatials/demo/spatial'
+                ),
+                LatchFile(
+                    'latch:///spatials/demo/spatial/tissue_positions_list.csv'
+                ),
             )
         ],
-    'project_name' : 'demo',
-    'genome' : Genome.hg38,
-    'run_table_id': '761',
-    'project_table_id': '779'
+        'project_name': 'demo',
+        'genome': Genome.hg38,
+        'run_table_id': '761',
+        'project_table_id': '779'
     },
 )
