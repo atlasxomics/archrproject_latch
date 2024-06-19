a6e8deb2820dd5738e9d67d4e3c80bad1a4d4e8c
James McGann <60373697+jpmcga@users.noreply.github.com>
2024-06-19 15:27:47 -0400

    fix: genelist parsing function

--- uiserver50by50/server.R
+++ uiserver50by50/server.R
@@ -708,11 +708,12 @@ scProp <- function(
 
 # Get gene list 
 scGeneList <- function(inp, inpGene) {
-  geneList = data.table(
-    gene = unique(trimws(strsplit(inp, ",|;|")[[1]])),
+  geneList <- data.table(
+    gene = unique(strsplit(inp, ", |;")[[1]]),
     present = TRUE
   )
-  geneList[!gene %in% names(inpGene)]$present = FALSE
+  geneList[!gene %in% names(inpGene)]$present <- FALSE
+  print(geneList)
   return(geneList)
 } 
 
